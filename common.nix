75031a5eec65dd2a8bb6b5889ad3e90c8ab2dad6
Sylvie Hacker <sylvie@karpador.xyz>
2024-04-28 13:47:59 +0200

    configure systemd-journald for my needs

--- common.nix
+++ common.nix
@@ -9,19 +9,29 @@
     vim htop
   ];
 
+  # we want a Sylvie everywhere
   users.users.sylvie = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
   };
 
+  # we also want ssh everywhere
   services.openssh.enable = true;
   services.openssh.settings = {
     PermitRootLogin = "without-password";
     PasswordAuthentication = false;
   };
 
+  # and tailscale
   services.tailscale.enable = true;
   systemd.services.tailscaled.after = [ "network-online.target" ];
+
+  # reasonable log config
+  services.journald.extraConfig = ''
+    SystemMaxUse=2G
+    SystemMaxFileSize=16M
+    SystemMaxFiles=32
+  '';
 
   nix = {
     gc = {
