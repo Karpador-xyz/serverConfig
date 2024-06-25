eee40e4f1f1dea60c314605b136c48b9a89e5e1e
Sylvie Hacker <sylvie@karpador.xyz>
2024-06-25 22:41:57 +0200

    kcloud: ipv6 config

--- kcloud/configuration.nix
+++ kcloud/configuration.nix
@@ -19,5 +19,19 @@
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
+  # ipv6
+  networking.interfaces.enp3s0 = {
+    ipv6 = {
+      addresses = [{
+        address = "2a0a:4cc0:1:1176::1";
+        prefixLength = 64;
+      }];
+    };
+  };
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "enp3s0";
+  };
+
   system.stateVersion = "23.11";
 }
