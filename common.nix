d4b2b7a3a77160e5334d7346be467e76788a6661
Sylvie Hacker <sylvie@karpador.xyz>
2024-07-15 10:39:50 +0200

    common.nix: add ssh keys to sylvie user

--- common.nix
+++ common.nix
@@ -13,6 +13,10 @@
   users.users.sylvie = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIBy1b6An4WzmHWnsUS1OfF/6lYc8bAzEjuFzQ+zFLEMi1Yyr117+7gus8fHvC7bg85R77vB32JbriRClafxmqLXRrAI9LLKWeUoSi28R6SVMiO3R3bGTTyIRpUFqTYa0aNycqfQiUbQVC1IbxeFPc+z3RSaZelKbPQd1EVh0JvcZ2vNwW3ohyj6sAE8utmGvWn6zGRdKAiITswaK+KScpMiZVBDi18A0T5lHbc1yJGsEW6InT+DKvXc4fWe5J56O9+asqztxgx0tXWxeP2+LuryV43LlHlsZ/Iv/YtMbbYI8d9OJh8LByKabr0BCAFd6YS55EZbiRngVJefqjqetD5p7M1fX7kz+MefospLtle2iwu8wj+IXvxkw0c9DGz22iMqCxUSdMKzUvNk6tii2vDYHW8lyT7gGPm1YebQ/TxOlJJr8tHR9YIISWEW7JilBVeUVjI+UzaC4rWTAciUI1P0cM+va7OGY/KLZoK1GQ+89L78TLazHFNkEr3TCe5Jc= sylvie@godemiche"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH4He6HobF5y8+viL1Fz9wWT+1Y3ptgZ6tpVQ+L3c9dq sylvie@tzuyu"
+    ];
   };
 
   # we also want ssh everywhere
