897ad1b71a820f57f15798611b94d03c404d1528
Sylvie Hacker <sylvie@karpador.xyz>
2025-06-21 15:26:40 +0200

    moo: ipv6 config

--- moo/configuration.nix
+++ moo/configuration.nix
@@ -8,5 +8,14 @@
   networking.hostName = "moo";
   networking.hostId = "7e35a6d4";
 
+  networking.interfaces.ens3.ipv6.addresses = [{
+    address = "2a03:4000:6:1be::1";
+    prefixLength = 64;
+  }];
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "ens3";
+  };
+
   system.stateVersion = "25.05";
 }
