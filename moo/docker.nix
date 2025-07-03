274ef5daf84a309dab1297f3fcb1ca0532ba773a
Sylvie Hacker <sylvie@karpador.xyz>
2025-07-03 21:06:31 +0200

    enable native ipv6 nat in docker as suggested by mailcow

--- moo/docker.nix
+++ moo/docker.nix
@@ -12,6 +12,12 @@
   virtualisation.docker = {
     enable = true;
     storageDriver = "zfs";
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "fd00:dead:beef:c0::/80";
+      experimental = true;
+      ip6tables = true;
+    };
   };
 
   # TODO: is there a way to link the normal generated config file there?
