189e225490116a022c61170efa72b7a25530f0b4
Sylvie Hacker <sylvie@karpador.xyz>
2025-07-12 08:27:24 +0200

    karp-zbox: improve backup pull script readability, also pull from moo

--- karp-zbox/backups.nix
+++ karp-zbox/backups.nix
@@ -23,6 +23,12 @@
       extraHostNames = [ "karp.lol" "152.53.16.62" ];
       publicKey = consts.hostKeys.kcloud-nix;
     };
+    moo = {
+      extraHostNames = [
+        "mail.karpador.xyz" "5.45.102.194" "2a03:4000:6:1be::1" "100.117.129.88"
+      ];
+      publicKey = consts.hostKeys.moo;
+    };
   };
   
   # finally, need a systemd oneshot unit that actually pulls snapshots using
@@ -36,16 +42,34 @@
       set -e -o pipefail
 
       # common options
-      OPTIONS="--no-privilege-elevation \
-        --sshkey=${config.age.secrets.kbackup-privkey.path} \
-        --no-sync-snap \
-        --recursive \
-        --skip-parent \
-        --delete-target-snapshots";
+      # we are using `zfs allow` to delegate the required permissions
+      OPTIONS="--no-privilege-elevation"
+      # use the ssh key provided by the secrets
+      OPTIONS="--sshkey=${config.age.secrets.kbackup-privkey.path} $OPTIONS"
+      # no sync snap - we are using sanoid on the source hosts
+      OPTIONS="--no-sync-snap $OPTIONS"
+      # the root source datasets are always gonna be empty
+      OPTIONS="--recursive --skip-parent $OPTIONS"
+      # don't keep old snapshots around
+      OPTIONS="--delete-target-snapshots $OPTIONS"
 
+      # SPECIFIC TO THIS HOST:
+      # force deletion of datasets in case there are no common snapshots
+      # this will be necessary at times because of the snapshot policy
+      # on the pgbackup dataset and because karp-zbox isn't reliably up
+      OPTIONS="--force-delete $OPTIONS";
+
+      # the everything host
+      echo syncing kcloud-nix...
       syncoid $OPTIONS \
-        kbackup@karp.lol:zroot/DATA \
+        kbackup@kcloud-nix:zroot/DATA \
         zroot/bckp/kcloud-nix/DATA
+
+      # the mail server
+      echo syncing moo...
+      syncoid $OPTIONS \
+        kbackup@moo:zroot/data \
+        zroot/bckp/moo/data
     '';
     serviceConfig = {
       Type = "oneshot";
