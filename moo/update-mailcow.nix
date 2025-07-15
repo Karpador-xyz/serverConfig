0395882487739eb11e7d3906a8bbb898d89f67c3
Sylvie Hacker <sylvie@karpador.xyz>
2025-07-15 11:49:41 +0200

    make mailcow update script a bit more robust

--- moo/update-mailcow.nix
+++ moo/update-mailcow.nix
@@ -9,11 +9,21 @@ in pkgs.writeShellApplication {
   text = ''
     cd /opt/mailcow-dockerized
 
+    # disable errexit - we want to allow the next two commands to fail
+    set +e
     if ./update.sh --check; then
       ./update.sh -f
+      RET=$?
       # if the script returns 2, the script has been replaced
       # and needs to be run again
-      [ $? -eq 2 ] && ./update.sh -f
+      if [ $RET -eq 2 ]; then
+        # re-enable errexit for this
+        set -e
+        ./update.sh -f
+      elif [ $RET -ne 0 ]; then
+        # forward error code
+        exit $RET
+      fi
     fi
   '';
 }
