a319acf9938c699c40c26db62405d23f88c141e8
Sylvie Hacker <sylvie@karpador.xyz>
2025-08-19 12:52:07 +0200

    boop: add kodi

--- boop/configuration.nix
+++ boop/configuration.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./disko.nix
     ./hardware.nix
     ../common.nix
   ];
+
+  users.users.kodi.isNormalUser = true;
+  services.cage.user = "kodi";
+  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  services.cage.enable = true;
 
   networking.hostName = "boop";
   networking.hostId = "8425e349";
