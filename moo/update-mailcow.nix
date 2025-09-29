75deb393d76f21de819b9108bc13f7438254933f
Sylvie Hacker <sylvie@karpador.xyz>
2025-09-29 13:33:40 +0200

    update-mailcow: next attempt at error handling

--- moo/update-mailcow.nix
+++ moo/update-mailcow.nix
@@ -6,18 +6,17 @@ in pkgs.writeShellApplication {
     coreutils gnused bashNonInteractive docker-client which curl
     gitMinimal iptables jq
   ];
+  bashOptions = [];
   text = ''
     cd /opt/mailcow-dockerized
 
-    # disable errexit - we want to allow the next two commands to fail
-    set +e
     if ./update.sh --check; then
       ./update.sh -f
       RET=$?
       # if the script returns 2, the script has been replaced
       # and needs to be run again
       if [ $RET -eq 2 ]; then
-        # re-enable errexit for this
+        # enable errexit for this
         set -e
         ./update.sh -f
       elif [ $RET -ne 0 ]; then
