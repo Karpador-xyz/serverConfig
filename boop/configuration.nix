bf15c915f288634c22f8e94113ae4505a9724af3
Sylvie Hacker <sylvie@karpador.xyz>
2025-10-16 18:43:35 +0200

    boop: playing around with some settings

--- boop/configuration.nix
+++ boop/configuration.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 let
-  # kodi = pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
-  #   vfs-sftp bluetooth-manager youtube orftvthek keymap
-  # ]);
-  retroarch = pkgs.retroarch.withCores (
-    cores: with cores; [
-      desmume dolphin citra
-    ]
-  );
+  kodi = pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
+    vfs-sftp bluetooth-manager youtube orftvthek keymap joystick
+  ]);
+  # retroarch = pkgs.retroarch.withCores (
+  #   cores: with cores; [
+  #     desmume dolphin citra
+  #   ]
+  # );
 in {
   imports = [
     ./disko.nix
@@ -17,10 +17,8 @@ in {
 
   users.users.kodi.isNormalUser = true;
   services.cage.user = "kodi";
-  services.cage.program = "${retroarch}/bin/retroarch";
+  services.cage.program = "${kodi}/bin/kodi-standalone";
   services.cage.enable = true;
-
-  systemd.services."cage-tty1".serviceConfig.Restart = "on-failure";
 
   networking.hostName = "boop";
   networking.hostId = "8425e349";
